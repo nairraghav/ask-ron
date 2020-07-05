065ae01947e74faf0c285c897d642c0bbd494d8c
Raghav Nair <nairraghav@hotmail.com>
2020-07-05 11:13:31 -0700

    Formatting

--- app/app.py
+++ app/app.py
@@ -3,10 +3,28 @@ from random import randint, choice
 from time import sleep
 
 APP = Flask("ask_ron")
-POSSIBLE_ANSWERS = ("As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
-                    "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
-                    "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
-                    "Yes.", "Yes – definitely.", "You may rely on it.")
+POSSIBLE_ANSWERS = (
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don’t count on it.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes – definitely.",
+    "You may rely on it.",
+)
 
 
 @APP.route("/", methods=["GET"])
